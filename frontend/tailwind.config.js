d8e9a5b5be2a7d0d74aca650ca7259c949021f4c
ICP.Ninja <noreply@icp.ninja>
2025-03-30 00:50:20 +0800

    ICP.Ninja export

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,37 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        blue: {
+          50: "var(--blue-50)",
+          100: "var(--blue-100)",
+          200: "var(--blue-200)",
+          300: "var(--blue-300)",
+          400: "var(--blue-400)",
+          500: "var(--blue-500)",
+          600: "var(--blue-600)",
+          700: "var(--blue-700)",
+          800: "var(--blue-800)",
+          900: "var(--blue-900)",
+          950: "var(--blue-950)",
+        },
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        background: "var(--background)",
+        light: "var(--light)",
+        border: "var(--border)",
+        button_bg: "var(--button-bg)",
+        button_hover: "var(--button-hover)",
+      },
+    },
+  },
+  plugins: [],
+}
+
